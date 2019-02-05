4bc6d9f5ee74fb47945cd791d175634e1c3a4f1c
Nate Scherer <376408+natescherer@users.noreply.github.com>
2019-02-05 13:30:14 -0500

    Update FromSource

--- AzureDeploymentTest.Dev.PSDeploy.ps1
+++ AzureDeploymentTest.Dev.PSDeploy.ps1
@@ -1,18 +1,18 @@
 Deploy AzureDeploymentTest {
-    By Task SetupAzureArtifactsRepo {
-        $AzureArtifactsSplat = @{
-            Name = "AzureArtifacts"
-            SourceLocation = "https://pkgs.dev.azure.com/natescherer/_packaging/NuGetFeed/nuget/v2"
-            PublishLocation    = "https://pkgs.dev.azure.com/natescherer/_packaging/NuGetFeed/nuget/v2"
-            InstallationPolicy = "Trusted"
-        }
-        Register-PSRepository @AzureArtifactsSplat
-    }
     By PSGalleryModule AzureArtifacts {
-        FromSource AzureDeploymentTest
+        FromSource .\src
         To AzureArtifacts
         WithOptions @{
             ApiKey = $env:AZUREARTIFACTSPAT
+        }
+        WithPreScript {
+            $AzureArtifactsSplat = @{
+                Name               = "AzureArtifacts"
+                SourceLocation     = "https://pkgs.dev.azure.com/natescherer/_packaging/NuGetFeed/nuget/v2"
+                PublishLocation    = "https://pkgs.dev.azure.com/natescherer/_packaging/NuGetFeed/nuget/v2"
+                InstallationPolicy = "Trusted"
+            }
+            Register-PSRepository @AzureArtifactsSplat            
         }
     }
 }
